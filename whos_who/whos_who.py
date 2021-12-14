f0556b0736cd45e280a1a65d8090d3905b0998d0
yoni <yoni.gozlan@student-cs.fr>
2021-12-14 18:05:27 +0100

    added comments to whos_who

--- whos_who/whos_who.py
+++ whos_who/whos_who.py
@@ -1,3 +1,7 @@
+'''
+Code to generate videos with boxes corresponding to the parquet file for the who's who exercise
+'''
+
 from copy import deepcopy
 from PIL import Image
 
@@ -97,23 +101,29 @@ if __name__ == "__main__":
     2 -> b
     '''
     series = pd.read_parquet("series.pq")
+    # Configurability
+    # change VECTOR_SET to "a", "b" or "c" and the number in VIDEO_SRC to test each vector set on each video
     VIDEO_SRC = "../samples/sample-0.mp4"
     VECTOR_SET = 'c'
     VIDEO_OUTPUT = "./results/test.avi"
+    # get only the vector set specified above
+    filter_col = [col for col in series if col.startswith(VECTOR_SET)]
+    series = series[filter_col]
+    series.rename(columns=lambda x: x[2:], inplace=True)
+    # setup the parameters to open, analyze the initial video, and save the modified video.
     cap = cv2.VideoCapture(VIDEO_SRC)
     ret, frame = cap.read()
     fourcc = cv2.VideoWriter_fourcc(*'MJPG')
     out = cv2.VideoWriter(VIDEO_OUTPUT, fourcc, 30.0, (frame.shape[1], frame.shape[0]))  # write the result to a video
-
-    filter_col = [col for col in series if col.startswith(VECTOR_SET)]
-    series = series[filter_col]
-    series.rename(columns=lambda x: x[2:], inplace=True)
+    # "299" shouldn't be hardcoded but I couldn't get the number of frame in a video, and the 3 samples are of the same length
     for frame_nb in range(299):
-        vector_dict = series.iloc[frame_nb].to_dict()
         print(frame_nb)
+        vector_dict = series.iloc[frame_nb].to_dict()
         ret, frame = cap.read()
+        # draw the head pose on the current frame
         frame_traced = draw_head_pose(vector_dict, frame)
         img_pil = Image.fromarray(frame_traced)
+        # write the modified frame to the output video
         out.write(frame_traced)
         key = cv2.waitKey(1) & 0xFF
         if key == ord("q"):
